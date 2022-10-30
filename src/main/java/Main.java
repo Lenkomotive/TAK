e0687e10ea9a2819afff531e36f87f85f0974e26
Mario Kaurin <mario-kaurin@outlook.com>
2022-10-30 20:55:25 +0100

    auto protobuf compile + grpc impl

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,17 @@ import algorithm.MinMax;
 import algorithm.Tree;
 import game.Action;
 import game.GameState;
+import netcode.GameComGrpc;
 
 public class Main {
     public static void main(String[] args) {
+
         GameState state = GameState.getDummyGameState3x3();
         Tree tree = MinMax.constructTree(state);
         Action action = MinMax.alphaBetaPrune(tree);
         System.out.println("baki is gay");
+
+
+
     }
 }
