4aa12a20d1566aa346251e2949d623ba33ea34d1
Mario Kaurin <mario-kaurin@outlook.com>
2022-10-31 21:34:51 +0100

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,6 +21,7 @@ public class Main {
 
     private static String matchToken;
     private static boolean beginningPlayer;
+
     public static void main(String[] args) throws InterruptedException {
         BasicConfigurator.configure(); //log4j
         client = new Client();
@@ -30,6 +31,7 @@ public class Main {
         checkAgreedTimeout();
         gameLoop();
     }
+
     private static void createMatch() {
         Netcode.MatchRequest request = client.createMatchRequest(BOARD_LENGTH, USER_TOKEN, GAME_TOKEN, TIMEOUT);
         Netcode.MatchResponse response = client.requestNewMatch(request);
@@ -38,6 +40,7 @@ public class Main {
         logger.info("Match token: " + matchToken);
         logger.info("Beginning player: " + beginningPlayer);
     }
+
     private static void waitForMatchToStart() throws InterruptedException {
         while(queryGameState().getGameStatus() == Netcode.GameStatus.MATCH_NOT_STARTED) {
             logger.info("Waiting for match to begin...");
@@ -45,6 +48,7 @@ public class Main {
         }
         logger.info("Match starts!");
     }
+
     private static void checkOpponentInfo() {
         Netcode.MatchIDPacket matchIDPacket = client.createMatchIdPacket(USER_TOKEN, matchToken);
         Netcode.OpponentInfoResponse opponentInfoResponse = client.getOpponentInfo(matchIDPacket);
@@ -53,6 +57,7 @@ public class Main {
                 + ", with a ELO value of: "
                 + opponentInfoResponse.getElo().getGroupElo());
     }
+
     private static void checkAgreedTimeout() {
         Netcode.MatchIDPacket matchIDPacket = client.createMatchIdPacket(USER_TOKEN, matchToken);
         logger.info("Agreed timeout: " + client.getTimeout(matchIDPacket));
@@ -70,20 +75,23 @@ public class Main {
             logger.info("Board length: " + state.getBoardLength());
             logger.info("Capstones: " + state.getRemainingCapstonesList());
             logger.info("Remaining stones: " + state.getRemainingStonesList());
+            //TODO calculate our turn!
             Tree tree = MinMax.constructTree(state);
             Tak.PlaceAction action = MinMax.alphaBetaPrune(tree);
-            //TODO calculate our turn!
             logger.info("\n");
         }
     }
+
     private static boolean matchIsRunning() {
         Netcode.GameStatus status = queryGameState().getGameStatus();
         return (status == Netcode.GameStatus.YOUR_TURN) || (status == Netcode.GameStatus.OPPONENTS_TURN);
     }
+
     private static boolean itIsOpponentsTurn() {
         Netcode.GameStatus status = queryGameState().getGameStatus();
         return status == Netcode.GameStatus.OPPONENTS_TURN;
     }
+
     private static Netcode.GameStateResponse queryGameState() {
         Netcode.MatchIDPacket matchIDPacket = client.createMatchIdPacket(USER_TOKEN, matchToken);
         return client.getGameState(matchIDPacket);
--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -23,26 +23,33 @@ public class Client {
                 .build();
         stub = GameComGrpc.newBlockingStub(channel);
     }
+
     public Netcode.MatchResponse requestNewMatch(Netcode.MatchRequest request) {
         logger.info("New Match requested");
         return stub.newMatch(request);
     }
+
     public Netcode.TurnResponse submitTurn(Netcode.TurnRequest turnRequest) {
         logger.info("Turn submitted");
         return stub.submitTurn(turnRequest);
     }
+
     public Netcode.GameStateResponse getGameState(Netcode.MatchIDPacket matchIDPacket) {
         return stub.getGameState(matchIDPacket);
     }
+
     public int getTimeout(Netcode.MatchIDPacket matchIDPacket) {
         return stub.getTimeout(matchIDPacket).getTimeoutSeconds();
     }
+
     public Netcode.OpponentInfoResponse getOpponentInfo(Netcode.MatchIDPacket matchIDPacket) {
         return stub.getOpponentInfo(matchIDPacket);
     }
+
     public Netcode.EloValues getEloValues(Netcode.IDPacket idPacket) {
         return stub.getElo(idPacket);
     }
+
     public Netcode.MatchRequest createMatchRequest(int boardLength, String userToken, String gameToken, int timeout) {
         Tak.GameParameter gameParameter = Tak.GameParameter.newBuilder().setBoardLength(boardLength).build();
         Netcode.MatchRequest.Builder matchRequestBuilder = Netcode.MatchRequest.newBuilder();
@@ -53,22 +60,24 @@ public class Client {
                 .setTakGameParameters(gameParameter)
                 .build();
     }
+
     public Netcode.TurnRequest createTurnRequest(Netcode.MatchIDPacket matchIDPacket, Tak.GameTurn gameTurn) {
         return  Netcode.TurnRequest.newBuilder()
                 .setMatchId(matchIDPacket)
                 .setTakGameTurn(gameTurn)
                 .build();
     }
+
     public Netcode.MatchIDPacket createMatchIdPacket(String userToken, String matchToken){
         return Netcode.MatchIDPacket.newBuilder()
                 .setUserToken(userToken)
                 .setMatchToken(matchToken)
                 .build();
     }
+
     public Netcode.IDPacket createIDPacket(String userToken) {
         return Netcode.IDPacket.newBuilder()
                 .setUserToken(userToken)
                 .build();
     }
-
 }
